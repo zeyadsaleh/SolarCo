545d24d12168257f2fa933b06ecf244a558554e2
Nouran Samy <nourans.96@yahoo.com>
2020-05-27 19:37:23 +0200

    Change user avatar option

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularTokenService } from 'angular-token';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -10,16 +11,47 @@ export class ProfileComponent implements OnInit {
 
   userData;
 
-  constructor(private tokenAuthService: AngularTokenService) { }
+  apiUrl:String = 'http://localhost:3000';
+
+  constructor(private tokenAuthService: AngularTokenService, private http: HttpClient) {}
 
   ngOnInit(): void {
+    // Get the data of the logged in user after validating token
     this.tokenAuthService.validateToken().subscribe(
       res => {
-        console.log(this.tokenAuthService.currentUserData)
         this.userData = this.tokenAuthService.currentUserData;
+
+        // Get all the data of the client with its avatar
+        this.http.get(`${this.apiUrl}/clients/${this.userData.id}`).subscribe(
+          res => {
+            if(res['avatar_url'])
+              this.userData.avatar = this.apiUrl + res['avatar_url']
+          },
+          error => console.log(error)
+        );
+
       },
       error => console.log(error)
-    );
+      );
+  }
+
+  uploadAvatar(ev) {
+    // Send the image as form data
+    let formData = new FormData();
+    formData.append('avatar', ev.target.files[0]);
+
+    // Specify the headers for form data not JSON
+    var headers = new HttpHeaders();
+    headers.set('Content-Type', 'multipart/form-data')
+
+    // Send the request and store the response in the avatar variable
+    this.http.put(`${this.apiUrl}/clients/avatar/${this.userData.id}`, formData, {headers: headers}).subscribe(
+      res => {
+        if(res['avatar_url'])
+          this.userData.avatar = this.apiUrl + res['avatar_url']
+      },
+      error => console.log(error)
+    )
   }
 
 }
