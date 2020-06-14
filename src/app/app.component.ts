cdeae73c7955b682ff5c3f93c27b4e73c70cdce7
zeyadsaleh <zeyadmostafasaleh@gmail.com>
2020-06-14 10:12:03 +0200

    slide to the top of the page in routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,4 +61,15 @@ export class AppComponent {
       this.isLoading = false;
     }
   }
+
+  onActivate(event) {
+        let scrollToTop = window.setInterval(() => {
+            let pos = window.pageYOffset;
+            if (pos > 0) {
+                window.scrollTo(0, pos - 20); // how far to scroll on each step
+            } else {
+                window.clearInterval(scrollToTop);
+            }
+        }, 16);
+    }
 }
